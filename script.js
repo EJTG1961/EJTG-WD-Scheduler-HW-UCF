c908356aae5724be9fb071f6c3a65d2d07caa67c
Edwin Toro-Garcia <ejtoro_garcia@icloud.com>
2022-04-07 19:42:10 -0400

    worked on Jscript, having moment js issue(warning), have to figure a solution

--- script.js
+++ script.js
@@ -8,42 +8,79 @@ $(document).ready(function(){
 })
 
 // listen for saveBtn clicks
-$(".saveBtn").on("click" function(){
+$(".saveBtn").on("click", function(){
   // Get to local.storage
     
-  var value = $(this).sibling(",information").val();
+  var value = $(this).siblings("information").val();
   var time = $(this).parent().attr("id");
   localStorage.setItem(time, value)
 
 })
 
-// Updating current time while changing time blocks' colors
-function colorUpdater() {
-  var currentHour = moment().hours();
+// // Updating current time while changing time blocks' colors
+// function colorUpdater() {
+//   var currentHour = moment().hours();
 
-  //creating variable for current time value
+//   //creating variable for current time value
 
-  //loop over timeblocks
-  $(".time-block").each(function() {
-    //need something to loop over time blocks
-    var blockTime =  //complete variable
-  })
+//   //loop over timeblocks
+//   $(".time-block").each(function() {
+//     //need something to loop over time blocks
+//     var blockTime =  //complete variable
+//   })
 
-  //conditional statement to check hours and apply correct time
-  if (blockTime < currentHour) {
+//   //conditional statement to check hours and apply correct time
+//   if (blockTime < currentHour) {
 
-} else if () {
+// } else if () {
 
+// } else {
+
+// }
+
+// }
+// //call funtion
+// colorUpdater();
+var checkTime = function () {
+   var hour = $(".hour");
+   console.log(hour);
+  var time = moment(hour, "LT");
+  console.log(time)
+
+
+
+  // for (i = 0; i < hour.length; i++) { 
+  //  console.log(hour[i].sibling('textarea')); 
+  // }
+hour.each(function(index){
+  console.log( index + ": " + $( this ).text() )
+  var time = $( this ).text().trim()
+  var textArea = $( this ).siblings("textArea");
+  textArea.removeClass(".present .past .future");
+  if (moment().isAfter(time)) {
+    textArea.addClass("past");
+} else if (moment().isBefore(time)) {
+    textArea.addClass("future");
 } else {
+    textArea.addClass("present");
+}
+moment.updateLocale('en', {
+  /**/
+});
+})
 
+
+  //remove any old classes from element
+ 
+
+  // apply new class if task is near/over due date
+ 
 }
 
-}
-//call funtion
-colorUpdater();
+checkTime();
 
 //another way to keep time updated
-var interval = setInterval(colorUpdater, 15000);
+var interval = setInterval(checkTime, 15000);
 
 // need saved data from local storage
 $("#hour-one .information").val(localStorage.getItem("#hour-one"));
